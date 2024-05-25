5fbfcc8207a110e3f2aae01ed4464b23cc02352e
NirmitKhrurana10 <nkhurana1010@gmail.com>
2024-05-24 21:43:29 -0400

    Adding [ages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,20 @@
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import About from "./pages/About";
+import Dashboard from "./pages/Dashboard";
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import SingUp from "./pages/SingUp";
+
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SingUp />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
