4f5f5cf3e3e2feb6f41821483da35e5a67b4c868
NirmitKhrurana10 <nkhurana1010@gmail.com>
2024-05-25 03:54:48 -0400

    Adding Header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,16 +4,20 @@ import Dashboard from "./pages/Dashboard";
 import Home from "./pages/Home";
 import SignIn from "./pages/SignIn";
 import SingUp from "./pages/SingUp";
+import Projects from "./pages/Projects";
+import Header from "./components/Header";
 
 export default function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SingUp />} />
+        <Route path="/projects" element={<Projects />} />
       </Routes>
     </BrowserRouter>
   );
