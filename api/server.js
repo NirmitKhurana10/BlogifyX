1179852a3fe93ef56c0043e141fedb7c25766aff
NirmitKhrurana10 <nkhurana1010@gmail.com>
2024-05-25 17:03:32 -0400

    Add middleware + util errorHandler function to handle errors

--- api/server.js
+++ api/server.js
@@ -20,6 +20,16 @@ mongoose
 app.use("/api/user", userRoute);
 app.use("/api/auth", authRoute);
 
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal server error";
+  res.status(statusCode).json({
+    success: false,
+    statusCode: statusCode,
+    message: message,
+  });
+});
+
 app.listen(process.env.PORT, () => {
   console.log(`Server listening on port ${process.env.PORT}`);
 });
