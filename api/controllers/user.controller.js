e227c1567777c32f990836a8a815aed702dd7eda
NirmitKhrurana10 <nkhurana1010@gmail.com>
2024-05-25 05:02:45 -0400

    Test API's anf folder creations

---
+++ api/controllers/user.controller.js
@@ -0,0 +1,3 @@
+export const test = (req, res) => {
+  res.json({ message: "A hello from user" });
+};
--- api/server.js
+++ api/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRoute from "./routes/user.route.js";
 
 const app = express();
 dotenv.config();
@@ -14,9 +15,7 @@ mongoose
     console.log("Connection failed !!", err);
   });
 
-app.get("/", (req, res) => {
-  res.send("Hello");
-});
+app.use("/api/user", userRoute);
 
 app.listen(process.env.PORT, () => {
   console.log(`Server listening on port ${process.env.PORT}`);
